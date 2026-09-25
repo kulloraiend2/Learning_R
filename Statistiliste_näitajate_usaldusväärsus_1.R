e8752edb7774a87e26c2ba0893a2799e8782ab0c
Kullo Raiend <kullo.raiend@gmail.com>
2026-09-25 20:41:55 +0300

    Väga lühikesed seeriad jäta välja

--- Statistiliste_näitajate_usaldusväärsus_1.R
+++ Statistiliste_näitajate_usaldusväärsus_1.R
@@ -858,6 +858,8 @@ find_ts_reliability <- function(ts_data) {
     # Eemalda seeriad, millel viimase perioodi väärtus puudub
     group_by_key() |>
     filter(max(Periood, na.rm = TRUE) == max_period) |> 
+    # Väga lühikesed seeriad jäta välja
+    filter(length(Periood) > 3)
     ungroup()
   
   
